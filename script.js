b8f324f0ad0cc364ad01405dd8c1bc026c6cc737
Victor Torres <victor68346@estudante.ifb.edu.br>
2026-03-14 20:54:37 -0300

    first commit

--- script.js
+++ script.js
@@ -7,14 +7,25 @@ const btnBuscarTodos = document.querySelector('#btnBuscarTodos');
 const novoTitulo = document.querySelector('#novoTitulo');
 const novoConteudo = document.querySelector('#novoConteudo');
 const btnNovoPost = document.querySelector('#btnNovoPost');
+let novoID;
+let ID;
 // Atualizar Post
-
+const btnAtualizar = document.querySelector('#btnAtualizar');
+const atualizarID = document.querySelector('#atualizarID');
+const atualizarTitulo = document.querySelector('#atualizarTitulo');
+// Deletar Post
+const btnDeletar = document.querySelector('#btnDeletar');
+const deletarID = document.querySelector('#deletarID');
 // Resultadosresultados
 const resultados = document.querySelector('#resultados');
+const mostrarResultados = document.querySelector('#mostrarResultados');
+
 // Eventos Botões
 btnBuscarPost.addEventListener('click', () => { buscarPostID(receberID.value); });
 btnBuscarTodos.addEventListener('click', () => { buscarTodos() });
-btnNovoPost.addEventListener('click', () => { criarNovoPost(novoTitulo.value, novoConteudo.value); console.log('a', novoTitulo.value, novoConteudo.value); })
+btnNovoPost.addEventListener('click', () => { criarNovoPost(novoTitulo.value, novoConteudo.value); console.log('a', novoTitulo.value, novoConteudo.value); });
+btnAtualizar.addEventListener('click', () => { atualizarPost(atualizarID.value, atualizarTitulo.value); });
+btnDeletar.addEventListener('click', () => { deletarPost(deletarID.value); });
 
 // Função Buscar Post por ID
 function buscarPostID(idPost) {
@@ -23,12 +34,15 @@ function buscarPostID(idPost) {
         .then((resultado) => resultado.json())
         .then((dados) => {
             console.log(dados);
+            mostrarResultados.classList.add('mostrar');
             resultados.innerHTML = `
-                <b>Título:</b> ${dados.title};
-                <b>Conteúdo:</b> ${dados.body}.
+                <h4><b>ID ${dados.id}</b></h4> 
+                <p><b>Título:</b> ${dados.title}.</p>
+                <p><b>Conteúdo:</b> ${dados.body}.</p>
                 `;
         })
         .catch(() => { resultados.innerHTML = 'Post não encontrado!' });
+    receberID.value = '';
 }
 
 // Função Buscar Todos Posts
@@ -37,6 +51,7 @@ function buscarTodos() {
         .then((resultaado) => resultaado.json())
         .then((dados) => {
             console.log(dados)
+            mostrarResultados.classList.add('mostrar');
             resultados.innerHTML = dados.map(dados => `
                 <h4><b>ID ${dados.id}</b></h4> 
                 <p><b>Título:</b> ${dados.title}.</p>
@@ -45,19 +60,90 @@ function buscarTodos() {
         });
 }
 
+// Buscar ID
+function buscarIdAtual() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((resultaado) => resultaado.json())
+        .then((dados) => {
+            let ultimo = dados.length;
+            novoID = parseInt(dados[ultimo - 1].id) + 1;
+            console.log("novo ID: ", novoID);
+        });
+    return novoID;
+}
+
 // Criar Novo Post
 function criarNovoPost(titulo, conteudo) {
-    console.log(titulo, conteudo);
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
+    console.log("Entrou", titulo, conteudo);
+    console.log(novoID);
+    if (titulo == '' || conteudo == '') {
+        alert("Informe um título ou conteúdo válido!")
+        novoConteudo.value = '';
+        novoTitulo.value = '';
+    } else {
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                id: novoID,
+                title: titulo,
+                body: conteudo,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((resultado) => resultado.json())
+            .then((dados) => {
+                console.log(dados)
+                mostrarResultados.classList.add('mostrar');
+                alert("Novo usuário adicionado com sucesso!");
+                resultados.innerHTML = `
+                    <h4><b>ID ${dados.id}</b></h4> 
+                   <p><b>Título:</b> ${dados.title}.</p>
+                   <p><b>Conteúdo:</b> ${dados.body}.</p>
+                `;
+            });
+        novoID += 1;
+        novoConteudo.value = '';
+        novoTitulo.value = '';
+    }
+}
+
+buscarIdAtual();
+
+// Atualizar Post
+function atualizarPost(idPost, novoTitulo) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`, {
+        method: 'PATCH',
         body: JSON.stringify({
-            title: titulo,
-            body: conteudo,
+            title: novoTitulo,
         }),
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         },
     })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+        .then((resultado) => resultado.json())
+        .then((dados) => {
+            console.log(dados);
+            mostrarResultados.classList.add('mostrar');
+            resultados.innerHTML = `
+                <h4><b>ID ${dados.id}</b></h4> 
+                <p><b>Título:</b> ${dados.title}.</p>
+                <p><b>Conteúdo:</b> ${dados.body}.</p>
+                `;
+            alert("Post atualizado com sucesso!");
+        });
+}
+
+// Deletar Post
+function deletarPost(idPost) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`, {
+        method: 'DELETE',
+    })
+        .then((resultado) => resultado.json())
+        .then((dados) => {
+            console.log(dados[idPost]);
+            alert("Post deletado com sucesso!");
+            deletarID.value = '';
+        });
 }
